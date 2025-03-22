6d72dbced4ace4764372e526256573a27e3e9692
samndambuki <sam.ndambuki08@gmail.com>
2025-03-22 14:32:44 +0300

    worked on unknown vs any types

---
+++ unknownvsany.ts
@@ -0,0 +1,14 @@
+//for unknown you have check the typer before using it
+
+// let data: unknown = "Hello Sam";
+// if (typeof data === "string") {
+//   console.log(data.toUpperCase());
+// }
+
+//for any you can use it without type checking
+let data: any = "Hello";
+console.log(data.toUpperCase());
+data = 10;
+
+//result in an error
+console.log(data.toUpperCase());
