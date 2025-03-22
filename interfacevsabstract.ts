3b937e61beee340df7b667fdeec4d29ed36408b3
samndambuki <sam.ndambuki08@gmail.com>
2025-03-22 20:21:20 +0300

    worked on interfaces vs mabstract classes

---
+++ interfacevsabstract.ts
@@ -0,0 +1,31 @@
+//implementation of an interface
+
+// interface Animal {
+//   name: string;
+//   makeSound(): void;
+// }
+
+// class Dog implements Animal {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+//   makeSound(): void {
+//     console.log("Bark");
+//   }
+// }
+
+//implementation of an abstract class
+// abstract class Animal {
+//   constructor(public name: string) {}
+//   abstract makeSound(): void;
+//   move(): void {
+//     console.log(`${this.name} is moving`);
+//   }
+// }
+
+// class Dog extends Animal {
+//   makeSound(): void {
+//     console.log("Bark");
+//   }
+// }
