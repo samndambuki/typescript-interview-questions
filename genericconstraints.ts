3e18c1306a93965e432cfecbfd9cf2d79d04ae4d
samndambuki <sam.ndambuki08@gmail.com>
2025-03-22 18:46:50 +0300

    worked on generic constraints

---
+++ genericconstraints.ts
@@ -0,0 +1,12 @@
+//restrict the types that can be passed to a generic function, class or interface
+//example
+function getProperty<T extends { name: string }>(obj: T): string {
+  return obj.name;
+}
+
+const person = { name: "Sam", age: 25 };
+console.log(getProperty(person));
+
+const numberValue = 42;
+//error
+// console.log(getProperyty(numberValue));
