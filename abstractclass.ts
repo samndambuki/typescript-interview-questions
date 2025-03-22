e44fbd76a7f00a63ed6c3d0dab36baadbf927dda
samndambuki <sam.ndambuki08@gmail.com>
2025-03-22 20:07:39 +0300

    worked on abstract classes

---
+++ abstractclass.ts
@@ -0,0 +1,21 @@
+//classes that cannot be instantiated and serves as a blue print for other classes
+abstract class Animal {
+  //must be implemented in subclasses
+  abstract makeSound(): void;
+  move(): void {
+    console.log("moving");
+  }
+}
+
+class Dog extends Animal {
+  makeSound(): void {
+    console.log("Bark");
+  }
+}
+
+const dog = new Dog();
+dog.makeSound();
+dog.move();
+
+//cannot instantiate abstract class
+// const animal = new Animal();
